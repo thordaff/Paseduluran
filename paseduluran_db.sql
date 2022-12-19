6069463bff283fda009384f620a658c38d7f4bf3
Thoriq Daffa <78408504+thordaff@users.noreply.github.com>
2022-12-20 06:06:27 +0700

    database add

--- paseduluran_db.sql
+++ paseduluran_db.sql
@@ -12,6 +12,28 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+-- Dumping structure for table paseduluran_db.menu
+CREATE TABLE IF NOT EXISTS `menu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(255) NOT NULL DEFAULT '0',
+  `kategori` enum('Manual Brew','Coffee','Latte','Tea Based','Traditional Drink','Food') NOT NULL,
+  `harga` int(50) NOT NULL DEFAULT 0,
+  `status` enum('Tersedia','Tidak Tersedia') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table paseduluran_db.menu: ~7 rows (approximately)
+/*!40000 ALTER TABLE `menu` DISABLE KEYS */;
+INSERT INTO `menu` (`id`, `nama`, `kategori`, `harga`, `status`) VALUES
+	(12, 'Americano', 'Coffee', 15000, 'Tersedia'),
+	(13, 'Pisang Goreng', 'Food', 6500, 'Tidak Tersedia'),
+	(14, 'Coffe Latte', 'Latte', 25000, 'Tersedia'),
+	(15, 'V60', 'Manual Brew', 15000, 'Tersedia'),
+	(16, 'Tea', 'Tea Based', 5000, 'Tidak Tersedia'),
+	(17, 'Wedhang Uwuh', 'Traditional Drink', 12000, 'Tidak Tersedia'),
+	(18, 'Susu Jahe', 'Traditional Drink', 15000, 'Tidak Tersedia');
+/*!40000 ALTER TABLE `menu` ENABLE KEYS */;
+
 -- Dumping structure for table paseduluran_db.user
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -23,7 +45,11 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table paseduluran_db.user: ~0 rows (approximately)
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`id`, `username`, `email`, `password`, `is_active`, `role_id`) VALUES
+	(1, 'Rayhan', 'rayhankusumo@paseduluran.com', '$2y$10$HjcfENphZrd5PzFO2Bk7Hetmf.txqqXVgr.U6TwOF85tWxDH44.pO', 1, 1);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 -- Dumping structure for table paseduluran_db.user_role
 CREATE TABLE IF NOT EXISTS `user_role` (
@@ -32,7 +58,13 @@ CREATE TABLE IF NOT EXISTS `user_role` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table paseduluran_db.user_role: ~2 rows (approximately)
+/*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
+INSERT INTO `user_role` (`id`, `role`) VALUES
+	(1, 'Admin'),
+	(2, 'Karyawan'),
+	(3, 'Member');
+/*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
